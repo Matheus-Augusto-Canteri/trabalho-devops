e84f7894ed4d31708e707d03b3cc3bc42256a3b0
MatheusCanteri <canterimatheu@gmail.com>
2025-03-19 20:25:09 -0300

    Outros Teste

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoTeste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 500)}
